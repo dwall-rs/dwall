fed9ba28847ef6822119d873ad1a5040f9e73b87
thepoy <thepoy@163.com>
2024-11-28 14:26:49 +0800

    chore(geo): Update logging levels for geolocation process
    This commit adjusts the logging levels for the geolocation process in `get_geo_position` function. The logging messages related to setting the desired accuracy, getting the geoposition asynchronously, and extracting details from the geoposition have been changed from `info!` to `trace!` to reduce noise in the logs for production use. An additional `info!` log has been added to output the current geoposition after it is successfully retrieved.

--- src-tauri/src/geo.rs
+++ src-tauri/src/geo.rs
@@ -16,7 +16,7 @@ pub fn get_geo_position() -> DwallResult<Position> {
     })?;
     debug!("Geolocator initialized successfully.");
 
-    info!("Setting desired accuracy to High...");
+    trace!("Setting desired accuracy to High...");
     geolocator
         .SetDesiredAccuracy(PositionAccuracy::High)
         .map_err(|e| {
@@ -25,40 +25,41 @@ pub fn get_geo_position() -> DwallResult<Position> {
         })?;
     debug!("Desired accuracy set to High successfully.");
 
-    info!("Getting geoposition asynchronously...");
+    trace!("Getting geoposition asynchronously...");
     let geoposition = geolocator.GetGeopositionAsync()?.get().map_err(|e| {
         error!("Failed to retrieve geoposition: {:?}", e);
         e
     })?;
     debug!("Geoposition retrieved successfully.");
 
-    info!("Extracting coordinate from geoposition...");
+    trace!("Extracting coordinate from geoposition...");
     let coordinate = geoposition.Coordinate().map_err(|e| {
         error!("Failed to extract coordinate from geoposition: {:?}", e);
         e
     })?;
     debug!("Coordinate extracted successfully.");
 
-    info!("Extracting point from coordinate...");
+    trace!("Extracting point from coordinate...");
     let point = coordinate.Point().map_err(|e| {
         error!("Failed to extract point from coordinate: {:?}", e);
         e
     })?;
     debug!("Point extracted successfully.");
 
-    info!("Extracting position from point...");
+    trace!("Extracting position from point...");
     let position = point.Position().map_err(|e| {
         error!("Failed to extract position from point: {:?}", e);
         e
     })?;
     debug!("Position extracted successfully.");
 
-    info!("Creating Position struct with latitude and longitude...");
+    trace!("Creating Position struct with latitude and longitude...");
     let result = Position {
         latitude: position.Latitude,
         longitude: position.Longitude,
     };
     debug!("Position struct created successfully.");
 
+    info!("Current geoposition: {:?}", result);
     Ok(result)
 }
