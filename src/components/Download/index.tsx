cbcbab9599b44015e91c04abcc3db13698a7f1d4
thepoy <thepoy@163.com>
2024-11-29 15:33:27 +0800

    feat: add Download component

---
+++ src/components/Download/index.tsx
@@ -0,0 +1,53 @@
+import { createEffect, createSignal, onMount } from "solid-js";
+import { LazyProgress } from "~/lazy";
+import "./index.scss";
+import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
+import { downloadThemeAndExtract } from "~/commands";
+import { useAppContext } from "~/context";
+
+interface ProgressPayload {
+  id: string;
+  progress: number;
+  total: number;
+}
+
+interface DownloadProps {
+  themeID: string;
+  onFinished: () => void;
+}
+
+const window = getCurrentWebviewWindow();
+
+const Download = (props: DownloadProps) => {
+  const { config } = useAppContext()!;
+  const [percent, setPercent] = createSignal<number>();
+
+  onMount(async () => {
+    const unlisten = await window.listen<ProgressPayload>(
+      "download-theme",
+      (e) => {
+        const { total, progress } = e.payload;
+        setPercent(Math.round((progress / total) * 1000) / 10);
+      },
+    );
+
+    await downloadThemeAndExtract(config()!, props.themeID);
+
+    props.onFinished();
+    setPercent();
+
+    unlisten();
+  });
+
+  createEffect(() => console.log(percent()));
+  //createEffect(() => {
+  //  if (percent() === 100) {
+  //    props.onFinished();
+  //    setPercent();
+  //  }
+  //});
+
+  return <LazyProgress class="download-progress" percent={percent()} />;
+};
+
+export default Download;
