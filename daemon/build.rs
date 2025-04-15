9e9138e73512f15b12e3d01a23e24e2008b9a8e4
thepoy <thepoy@163.com>
2025-04-15 16:08:11 +0800

    feat: update copyright information and configure winresource for build script

--- daemon/build.rs
+++ daemon/build.rs
@@ -8,6 +8,10 @@ fn main() -> io::Result<()> {
         if env::var_os("CARGO_CFG_WINDOWS").is_some() {
             WindowsResource::new()
                 .set_icon("../src-tauri/icons/icon.ico")
+                .set(
+                    "LegalCopyright",
+                    "Copyright (C) 2025 thep0y. All rights reserved.",
+                )
                 .compile()?;
         }
     }
