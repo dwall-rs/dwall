3472d7ba39338d1b0fdeac69eeda96ebefd7eed9
thepoy <thepoy@163.com>
2026-05-11 10:39:47 +0800

    refacto: uniformly move rust packages to crates directory.

--- crates/dwall/build.rs
+++ crates/dwall/build.rs
@@ -11,7 +11,7 @@ fn main() -> io::Result<()> {
 
         if env::var_os("CARGO_CFG_WINDOWS").is_some() {
             WindowsResource::new()
-                .set_icon("../src-tauri/icons/icon.ico")
+                .set_icon("../dwall-settings/icons/icon.ico")
                 .set(
                     "LegalCopyright",
                     "Copyright (C) 2025 thep0y. All rights reserved.",
